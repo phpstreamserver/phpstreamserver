9e44d39e37311f57c7bf55a830884ac1c319c655
Anton <anton.z@live.com>
2024-11-19 01:35:16 +0800

    Split test

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $mbConfig): void {
-    $mbConfig->packageDirectories([__DIR__ . '/src/Server']);
-    $mbConfig->packageDirectories([__DIR__ . '/src/Plugins']);
+    $mbConfig->packageDirectories([
+        __DIR__ . '/src/Server',
+        __DIR__ . '/src/Plugins',
+    ]);
 };
