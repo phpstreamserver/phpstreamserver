1464ed7a7c76db905a106360380326864bf6d48e
Anton <anton.z@live.com>
2026-07-15 00:24:55 +0800

    Increase sleep timeout in tests bootstrap file

--- tests/data/bootstrap.php
+++ tests/data/bootstrap.php
@@ -16,7 +16,7 @@ function phpss_start(): void
 {
     $descriptor = [1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
     $process = \proc_open(\phpss_create_command('start -d'), $descriptor, $pipes);
-    \usleep(100000);
+    \usleep(300000);
     \proc_close($process);
 }
 
