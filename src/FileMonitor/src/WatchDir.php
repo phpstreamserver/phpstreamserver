6769a69e6c3e7036d535f3f9bb802936df9b9dd3
Anton <anton.z@live.com>
2026-07-30 11:28:27 +0800

    WatchDir parameters documentation

--- src/FileMonitor/src/WatchDir.php
+++ src/FileMonitor/src/WatchDir.php
@@ -6,6 +6,12 @@ namespace PHPStreamServer\Plugin\FileMonitor;
 
 final readonly class WatchDir
 {
+    /**
+     * @param string $sourceDir directory to watch
+     * @param array<string> $filePatterns glob patterns for filenames to watch
+     * @param bool $recursive whether to watch subdirectories
+     * @param bool $invalidateOpcache whether to invalidate cached scripts on reload
+     */
     public function __construct(
         public string $sourceDir,
         public array $filePatterns = ['*'],
