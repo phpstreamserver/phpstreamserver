5e39ac84dd5d196cd9a42fbad26f99c25d1bc7e5
Anton <anton.z@live.com>
2026-08-16 04:07:00 +0800

    Fix4

--- src/FileMonitor/src/Internal/FSEventsFileWatcher.php
+++ src/FileMonitor/src/Internal/FSEventsFileWatcher.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PHPStreamServer\Plugin\FileMonitor\Internal;
 
 use PHPStreamServer\Plugin\FileMonitor\Internal\FFIBindings\FSEvents;
+use PHPStreamServer\Plugin\FileMonitor\WatchRule;
 use Revolt\EventLoop;
 
 /**
@@ -55,12 +56,18 @@ final class FSEventsFileWatcher extends AbstractFileWatcher
     {
         $path = \rtrim($path, '/');
         $isDirChange = ($flags & (FSEvents::EVENT_FLAG_ITEM_IS_DIR | FSEvents::EVENT_FLAG_ITEM_IS_SYMLINK)) !== 0 && ($flags & (FSEvents::EVENT_FLAG_ITEM_REMOVED | FSEvents::EVENT_FLAG_ITEM_RENAMED)) !== 0;
+        $isCreatedDir = ($flags & FSEvents::EVENT_FLAG_ITEM_CREATED) !== 0 && \is_dir($path);
         $isSourceDirChange = ($flags & (FSEvents::EVENT_FLAG_ITEM_REMOVED | FSEvents::EVENT_FLAG_ITEM_RENAMED | FSEvents::EVENT_FLAG_ROOT_CHANGED)) !== 0;
 
         foreach ($this->rules as $rule) {
             $sourceDir = \rtrim($rule->sourceDir, '/');
 
             if ($isSourceDirChange && $path === $sourceDir) {
+                $this->scheduleReload($rule->invalidateOpcache);
+                continue;
+            }
+
+            if ($isCreatedDir && $path === $sourceDir && $this->containsMatchingFile($rule, $path)) {
                 $this->scheduleReload($rule->invalidateOpcache);
                 continue;
             }
@@ -72,8 +79,32 @@ final class FSEventsFileWatcher extends AbstractFileWatcher
 
             if ($isDirChange && $rule->recursive && \str_starts_with($path, $sourceDir . '/')) {
                 $this->scheduleReload($rule->invalidateOpcache);
+                continue;
+            }
+
+            if ($isCreatedDir && $rule->recursive && \str_starts_with($path, $sourceDir . '/') && $this->containsMatchingFile($rule, $path)) {
+                $this->scheduleReload($rule->invalidateOpcache);
             }
         }
+    }
+
+    private function containsMatchingFile(WatchRule $rule, string $path): bool
+    {
+        if ($rule->recursive) {
+            $directory = new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS);
+            $iterator = new \RecursiveIteratorIterator($directory, flags: \RecursiveIteratorIterator::CATCH_GET_CHILD);
+        } else {
+            $iterator = new \FilesystemIterator($path, \FilesystemIterator::SKIP_DOTS);
+        }
+
+        foreach ($iterator as $file) {
+            /** @var \SplFileInfo $file */
+            if ($file->isFile() && $this->isPatternMatches($rule, $file->getPathname())) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
