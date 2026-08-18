ef6f7d29e5eefe2d8fc73b916f51fb15b25c1e16
Anton <anton.z@live.com>
2026-08-19 01:42:03 +0800

    test bootstrap file fixes. Handle test server stop on SIGINT properly. Simplify start command

--- tests/data/bootstrap.php
+++ tests/data/bootstrap.php
@@ -7,6 +7,13 @@ include __DIR__ . '/../../vendor/autoload.php';
 \phpss_start();
 \register_shutdown_function(\phpss_stop(...));
 
+\pcntl_async_signals(true);
+\pcntl_signal(SIGINT, static function (): void {
+    \phpss_stop();
+    echo "\n";
+    exit(128 + SIGINT);
+});
+
 function phpss_create_command(string $command): string
 {
     return \sprintf('exec %s -d ffi.enable=true %s/server.php %s', PHP_BINARY, __DIR__, $command);
@@ -14,13 +21,13 @@ function phpss_create_command(string $command): string
 
 function phpss_start(): void
 {
-    $descriptor = [1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
-    $process = \proc_open(\phpss_create_command('start -d'), $descriptor, $pipes);
-    \usleep(200000);
-    \proc_close($process);
+    \exec(\phpss_create_command('start -d') . ' > /dev/null 2>&1', $output, $exitCode);
+    if ($exitCode !== 0) {
+        throw new \RuntimeException('Failed to start server');
+    }
 }
 
 function phpss_stop(): void
 {
-    \shell_exec(\phpss_create_command('stop'));
+    \exec(\phpss_create_command('stop') . ' > /dev/null 2>&1');
 }
